49747142269d5ae102fdf6834fd96f8c77b30a73
Xiaoxi Pang <pangxiaoxi@gmail.com>
2020-03-05 21:23:49 -0500

    3.4.2 add page titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
     def home
     end
+
+    def about
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'application#hello'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 end
